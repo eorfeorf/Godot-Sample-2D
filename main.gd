7057fdbba2cdea58f66769bc982e66634663fa4f
EF <ef256@gmail.com>
2024-08-18 13:16:13 +0900

    HUDを実装

--- main.gd
+++ main.gd
@@ -5,27 +5,28 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	#new_game()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_player_hit() -> void:
-	pass # Replace with function body.
 	
 	
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	pass
 	
 func new_game() -> void:
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 	pass
 
 
@@ -37,6 +38,7 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 	pass # Replace with function body.
 
 
