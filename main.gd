d0aad2f5c88ff3a38cb10fa16b4efd156f865e8e
EF <ef256@gmail.com>
2024-08-18 12:30:14 +0900

    敵の挙動を実装

---
+++ main.gd
@@ -0,0 +1,59 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	new_game()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_hit() -> void:
+	pass # Replace with function body.
+	
+	
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game() -> void:
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	pass
+
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+	pass # Replace with function body.
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+	pass # Replace with function body.
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	var mob_spawn_locatioin = $MobPath/MobSpawnLocation
+	mob_spawn_locatioin.progress_ratio = randf()
+	
+	var direction = mob_spawn_locatioin.rotation + PI / 2
+	
+	mob.position = mob_spawn_locatioin.position
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+	pass # Replace with function body.
