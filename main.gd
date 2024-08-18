9051b5ba637713f0fb1d7cf76bd43a0f17bcb70d
EF <ef256@gmail.com>
2024-08-18 13:26:56 +0900

    BGMと効果音を追加

--- main.gd
+++ main.gd
@@ -18,6 +18,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	pass
 	
 func new_game() -> void:
@@ -27,6 +29,7 @@ func new_game() -> void:
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 	pass
 
 
